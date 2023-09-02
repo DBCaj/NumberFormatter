0142a4314f5f77214161cb821ae83f1f76ec3666
DBCaj <fnom098@gmail.com>
2023-09-02 23:27:37 +0800

    Trasnformed web route in to cleanier code

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,21 @@ use App\Http\Controllers\NumberFormatterController;
 Route::view('/', 'layouts.number-form')->name('number.form')->name('number.form');
 Route::get('/num-format', [NumberFormatterController::class, 'numFormat'])->name('number.format');
 
-////////////// Formats - Start ///////////////
-Route::view('/currency-true-format', 'formats.currency-true-format');
-Route::view('/currency-false-format', 'formats.currency-false-format');
-Route::view('/decimal-true-format', 'formats.decimal-true-format');
-Route::view('/decimal-false-format', 'formats.decimal-false-format');
-Route::view('/percent-true-format', 'formats.percent-true-format');
-Route::view('/percent-false-format', 'formats.percent-false-format');
-Route::view('/spellout-true-format', 'formats.spellout-true-format');
-Route::view('/spellout-false-format', 'formats.spellout-false-format');
-Route::view('/padding-true-format', 'formats.padding-true-format');
-Route::view('/padding-false-format', 'formats.padding-false-format');
-////////////// Formats - End ///////////////
+
+// Formats Routes
+$formatRoutes = [
+    'currency-true-format',
+    'currency-false-format',
+    'decimal-true-format',
+    'decimal-false-format',
+    'percent-true-format',
+    'percent-false-format',
+    'spellout-true-format',
+    'spellout-false-format',
+    'padding-true-format',
+    'padding-false-format',
+];
+
+foreach ($formatRoutes as $route) {
+    Route::view("/$route", "formats.$route");
+}
